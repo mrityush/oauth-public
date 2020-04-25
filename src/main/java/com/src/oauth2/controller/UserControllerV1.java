b744379251d63d3ef18ba7c04bdf8e16103b2677
tecklabs <teckacademy@gmail.com>
2020-04-25 15:12:59 +0530

    sending user dto in user profile api call...

--- src/main/java/com/src/oauth2/controller/UserControllerV1.java
+++ src/main/java/com/src/oauth2/controller/UserControllerV1.java
@@ -2,13 +2,13 @@ package com.src.oauth2.controller;
 
 import com.src.oauth2.command.UserCO;
 import com.src.oauth2.command.UserLoginCO;
-import com.src.oauth2.domain.User;
 import com.src.oauth2.dto.UserDTO;
 import com.src.oauth2.exception.ResourceNotFoundException;
 import com.src.oauth2.repositories.UserRepository;
 import com.src.oauth2.security.CurrentUser;
 import com.src.oauth2.security.UserPrincipal;
 import com.src.oauth2.services.interfaces.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,12 +24,14 @@ public class UserControllerV1 {
 	private UserRepository userRepository;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@GetMapping("/user/me")
 	@PreAuthorize("hasRole('USER')")
-	public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
-		return userRepository.findById(userPrincipal.getId())
-				.orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+	public UserDTO getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+		return modelMapper.map(userRepository.findById(userPrincipal.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId())), UserDTO.class);
 	}
 
 	@PostMapping("/v1/register")
